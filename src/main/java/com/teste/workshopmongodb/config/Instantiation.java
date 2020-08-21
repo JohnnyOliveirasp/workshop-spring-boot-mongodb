ef3a13521ae9983bb97512e17c2c38eb41414cdb
Johnny Marcos de Oliveira <johnny.oliveirasp@gmail.com>
2020-08-21 13:14:04 -0300

    Referenciando os posts do usuario

--- src/main/java/com/teste/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/teste/workshopmongodb/config/Instantiation.java
@@ -1,4 +1,5 @@
 package com.teste.workshopmongodb.config;
+
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
@@ -12,6 +13,7 @@ import com.teste.workshopmongodb.domain.User;
 import com.teste.workshopmongodb.dto.AuthorDTO;
 import com.teste.workshopmongodb.repository.PostRepository;
 import com.teste.workshopmongodb.repository.UserRepository;
+
 
 
 @Configuration
@@ -42,6 +44,9 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userReposiroty.save(maria);
 	}
 
 }
--- src/main/java/com/teste/workshopmongodb/domain/User.java
+++ src/main/java/com/teste/workshopmongodb/domain/User.java
@@ -1,14 +1,15 @@
 package com.teste.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
-@Document(collection = "users")
-public class User implements Serializable{
-
+@Document(collection="user")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -16,8 +17,18 @@ public class User implements Serializable{
 	private String name;
 	private String email;
 	
-	public User() {}
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
+	public User() {
+	}
+
+	public User(String id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
 
 	public String getId() {
 		return id;
@@ -43,13 +54,14 @@ public class User implements Serializable{
 		this.email = email;
 	}
 
-	public User(String id, String name, String email) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
+	public List<Post> getPosts() {
+		return posts;
 	}
-	
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
